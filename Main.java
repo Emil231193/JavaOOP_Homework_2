61b9b915d0fb0d691cfd0e6f1be85400ce9305af
Emil Yagudin <98709344+Emil231193@users.noreply.github.com>
2023-05-08 22:19:13 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<BaseHero> heroesOne = new ArrayList<>();
+        ArrayList<BaseHero> heroesTwo = new ArrayList<>();
+        CreateHeroes(heroesOne, 1);
+        CreateHeroes(heroesTwo, 2);
+
+        heroesOne.forEach(n -> System.out.print(n.getInfo() + ", "));
+        heroesOne.forEach(n -> n.step(heroesOne));
+        System.out.println();
+        System.out.println();
+        heroesTwo.forEach(n -> System.out.print(n.getInfo() + ", "));
+        heroesTwo.forEach(n -> n.step(heroesTwo));
+    }
+
+    public static void CreateHeroes(ArrayList<BaseHero> heroes, int mode){
+        String[] heroes_name = new String[20];
+        for (int i = 1; i < 21; i++) {
+            heroes_name[i-1] = "Player" + i;
+        }
+        if (mode == 1){
+            for (int k = 0; k < 10; k++) {
+                int number = randomInt(4);
+                if (number == 0) {
+                    heroes.add(new Farmer(heroes_name[k], "Farmer"));
+                }
+                else if (number == 1) {
+                    heroes.add(new Rogue(heroes_name[k], "Rogue"));
+                }
+                else if (number == 2) {
+                    heroes.add(new Sniper(heroes_name[k], "Sniper"));
+                }
+                else if (number == 3) {
+                    heroes.add(new Mage(heroes_name[k], "Mage"));
+                }
+            }
+        } else if (mode == 2) {
+            for (int k = 10; k < 20; k++) {
+                int number = randomInt(4);
+                if (number == 0) {
+                    heroes.add(new Crossbowman(heroes_name[k], "Crossbowman"));
+                }
+                else if (number == 1) {
+                    heroes.add(new Monk(heroes_name[k], "Monk"));
+                }
+                else if (number == 2) {
+                    heroes.add(new Spearman(heroes_name[k], "Spearman"));
+                }
+                else if (number == 3) {
+                    heroes.add(new Farmer(heroes_name[k], "Farmer"));
+                }
+            }
+        }
+    }
+
+    public static int randomInt(int num){
+        ThreadLocalRandom randomNum = ThreadLocalRandom.current();
+        int temp = randomNum.nextInt(num);
+        return temp;
+    }
+}
